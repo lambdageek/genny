b9739c7c69972a674c6d9e799457821330603601
Aleksey Kliger <aleksey@lambdageek.org>
2021-08-02 19:49:46 -0400

    Initial Commit

---
+++ Alekseyzor/Alekseyzor.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+
+namespace Alekseyzor
+{
+    [Generator]
+    public class Alekseyzor : ISourceGenerator
+    {
+
+        public void Execute(GeneratorExecutionContext context)
+        {
+            // Code generation goes here
+            context.AddSource ("myContent", @"
+using System;
+public class IAmHero {
+    public static void DoSomething() {
+        Func<string,string> fn = (s) => s + ""!"";
+        Console.WriteLine(fn (""Hello, I am a hero""));
+    }
+}
+            ");
+        }
+
+        public void Initialize(GeneratorInitializationContext context)
+        {
+            // No initialization required for this one
+        }
+    }
+}
---
+++ Sample/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Reflection;
+
+namespace Sample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<string,string> fn = (s) => s + "!";
+            var assm = typeof(Program).Assembly;
+            var ty = assm.GetType("IAmHero");
+            Console.WriteLine (fn($"Got {ty.FullName}"));
+            var mi = ty.GetMethod("DoSomething", BindingFlags.Public | BindingFlags.Static);
+            mi.Invoke(null, Array.Empty<object>());
+        }
+    }
+}
