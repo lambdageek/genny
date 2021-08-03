11492305c88673998356185186a36d2901e2fa09
Aleksey Kliger <alklig@microsoft.com>
2021-08-03 13:29:41 -0400

    Use an incremental generator

--- Alekseyzor/Alekseyzor.cs
+++ Alekseyzor/Alekseyzor.cs
@@ -1,31 +1,34 @@
 ﻿using System;
+using System.Collections.Generic;
 using Microsoft.CodeAnalysis;
+using System.Linq;
 using Microsoft.CodeAnalysis.CSharp;
 
 namespace Alekseyzor
 {
     [Generator]
-    public class Alekseyzor : ISourceGenerator
+    public class Alekseyzor : IIncrementalGenerator
     {
+        private readonly DiagnosticDescriptor _genDescriptor;
 
-        public void Execute(GeneratorExecutionContext context)
-        {
-            // Code generation goes here
-            context.AddSource ("myContent", @"
-using System;
-public class IAmHero {
-    public IAmHero () { }
-    public void DoSomething() {
-        Func<string,string> fn = static (s) => s + ""!"";
-        Console.WriteLine(fn (""Hello, I am a hero""));
-    }
-}
-            ");
+        public Alekseyzor () {
+            _genDescriptor = new DiagnosticDescriptor ("AZ0001", "Generating", "Generating {0}", "Alekseyzor", DiagnosticSeverity.Info, true);
         }
 
-        public void Initialize(GeneratorInitializationContext context)
+        public void Initialize(IncrementalGeneratorInitializationContext context)
         {
+            System.IO.File.AppendAllText("/tmp/az.txt", string.Format("Running at {0}\n", DateTime.Now));
             // No initialization required for this one
+
+            var texts = context.AdditionalTextsProvider;
+
+            context.RegisterSourceOutput(texts, (context, addtext) => {
+                var bp = System.IO.Path.GetFileName(addtext.Path);
+                context.ReportDiagnostic(Diagnostic.Create(_genDescriptor, null, bp));
+                var str = addtext.GetText().ToString();
+                // Code generation goes here
+                context.AddSource (bp + ".cs", str);
+            });
         }
     }
 }
