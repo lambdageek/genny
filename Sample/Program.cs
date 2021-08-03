7741ae978c7391a846ae210e4c0aeac8f56d2f58
Aleksey Kliger <alklig@microsoft.com>
2021-08-03 13:32:59 -0400

    Also add a line hidden pragma to the main program

--- Sample/Program.cs
+++ Sample/Program.cs
@@ -7,14 +7,20 @@ namespace Sample
     {
         static void Main(string[] args)
         {
-            Func<string,string> fn = (s) => s + "!";
             var assm = typeof(Program).Assembly;
             var ty = assm.GetType("IAmHero");
-            Console.WriteLine (fn($"Got {ty.FullName}"));
+            Console.WriteLine (Transform($"Got {ty.FullName}"));
             var ci = ty.GetConstructor(Array.Empty<Type>());
             var obj = ci.Invoke(Array.Empty<object>());
             var mi = ty.GetMethod("DoSomething", BindingFlags.Public | BindingFlags.Instance);
             mi.Invoke(obj, Array.Empty<object>());
+        }
+
+// Comment out the line pragma to get EnC Lambda info
+#line hidden
+        public static string Transform(string s) {
+            Func<string,string> fn = (s) => s + "!";
+            return fn(s);
         }
     }
 }
